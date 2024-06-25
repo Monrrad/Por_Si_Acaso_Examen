33f6aac12be6c204d196c1fdd5909f94aa4a41ab
Monrrad <ramondavidmc@ufps.edu.co>
2024-06-24 20:46:46 -0500

    Update Semillero

--- src/main/java/co/edu/ufps/proyecto/controllers/SemilleroController.java
+++ src/main/java/co/edu/ufps/proyecto/controllers/SemilleroController.java
@@ -1,4 +1,30 @@
 package co.edu.ufps.proyecto.controllers;
 
+import co.edu.ufps.proyecto.entities.Semillero;
+import co.edu.ufps.proyecto.models.SemilleroDTO;
+import co.edu.ufps.proyecto.services.SemilleroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/semillero")
 public class SemilleroController {
+
+@Autowired
+    SemilleroService semilleroService;
+
+@PutMapping("/{id}")
+public ResponseEntity<Semillero> update(@PathVariable Integer id, @RequestBody SemilleroDTO semilleroDTO) {
+
+    Semillero updateSemillero = semilleroService.updateSemillero(id, semilleroDTO);
+    if (updateSemillero == null) {
+        return ResponseEntity.status(404).body(null);
+    }
+    return ResponseEntity.ok(updateSemillero);
+
 }
+
+}
+
+
--- src/main/java/co/edu/ufps/proyecto/models/SemilleroDTO.java
+++ src/main/java/co/edu/ufps/proyecto/models/SemilleroDTO.java
@@ -1,8 +1,12 @@
 package co.edu.ufps.proyecto.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SemilleroDTO {
 
     private int id;
